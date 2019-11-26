3a6223a43aeb60e571046c62603f0ef0978e1c1f
Alkavarghese <55538698+Alkavarghese@users.noreply.github.com>
2019-11-26 18:09:53 +0530

    Add files via upload

---
+++ form_validation_using_js/pas.js
@@ -0,0 +1,38 @@
+
+function ValidatePassword() {
+
+  var rules = [{
+      Pattern: "[A-Z]",
+      Target: "UpperCase"
+    },
+    {
+      Pattern: "[a-z]",
+      Target: "LowerCase"
+    },
+    {
+      Pattern: "[0-9]",
+      Target: "Numbers"
+    },
+    {
+      Pattern: "[!@@#$%^&*]",
+      Target: "Symbols"
+    }
+  ];
+
+  var password = $(this).val();
+
+
+  $("#Length").removeClass(password.length > 8 ? "glyphicon-remove" : "glyphicon-ok");
+  $("#Length").addClass(password.length > 8 ? "glyphicon-ok" : "glyphicon-remove");
+
+
+  for (var i = 0; i < rules.length; i++) {
+
+    $("#" + rules[i].Target).removeClass(new RegExp(rules[i].Pattern).test(password) ? "glyphicon-remove" : "glyphicon-ok");
+    $("#" + rules[i].Target).addClass(new RegExp(rules[i].Pattern).test(password) ? "glyphicon-ok" : "glyphicon-remove");
+      }
+    }
+
+    $(document).ready(function() {
+      $("#pass").on('keypress', ValidatePassword)
+    });
